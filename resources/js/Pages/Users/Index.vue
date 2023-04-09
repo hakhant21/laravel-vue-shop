a62c476be7b008a503f9cefa1601fbb1994bb583
hakhant21 <hakhant21@gmail.com>
2023-04-09 11:46:52 +0630

    format user index file

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -18,8 +18,8 @@ import PrimaryButton from '@/Components/PrimaryButton.vue';
 const nameRef = ref(null);
 const emailRef = ref(null);
 const roleRef = ref(null);
-const passwordRef =ref(null);
-const selectedRole = ref(null)
+const passwordRef = ref(null);
+const selectedRole = ref(null);
 
 const confirmingUserCreation = ref(false);
 const confirmShowEditModal = ref(false);
@@ -46,16 +46,16 @@ const props = defineProps({
 const form = useForm({
     name: '',
     email: '',
-    role:'',
-    password:'',
-    sub_role:'',
-    vendor_name:''
+    role: '',
+    password: '',
+    sub_role: '',
+    vendor_name: ''
 });
 
 const formEdit = useForm({
-    user_id:'',
-    sub_role:'',
-    vendor_id:''
+    user_id: '',
+    sub_role: '',
+    vendor_id: ''
 
 });
 
@@ -76,15 +76,15 @@ const createUser = () => {
 };
 
 const editUser = () => {
-    formEdit.post(route('users:update',{
+    formEdit.post(route('users:update', {
         user: formEdit.user_id
     }), {
         preserveScroll: true,
         onSuccess: () => {
-            closeEditModal()
+            closeEditModal();
             toast.add({
                 message: "Successful update !"
-            })
+            });
         },
     });
 };
@@ -100,10 +100,10 @@ const closeModal = () => {
 
 const showEditModal = (user) => {
     formEdit.user_id = user.data.id;
-    formEdit.vendor_id = _.map(user.data.uservendor,'id').join(',');
-    formEdit.sub_role = _.map(user.data.vendor,'sub_role').join(',');
+    formEdit.vendor_id = _.map(user.data.uservendor, 'id').join(',');
+    formEdit.sub_role = _.map(user.data.vendor, 'sub_role').join(',');
     confirmShowEditModal.value = true;
-    
+
 };
 
 const closeEditModal = () => {
@@ -125,30 +125,32 @@ const closeEditModal = () => {
             <table>
                 <thead>
                     <tr>
-                        <th >No</th>
-                        <th >Name</th>
-                        <th >Email</th>
-                        <th >Role</th>
-                        <th >Sub Role</th>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Sub Role</th>
                         <th>Vendor Name</th>
-                        <th >Action</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="(user,index) in props.users.data" :key="index">
-                                    <td>{{ ++index }}</td>     
-                                    <td data-label="Name">{{ user.data.name }} </td>
-                                    <td data-label="Email">{{ user.data.email }}</td>
-                                    <td data-label="Role">{{ user.data.role }}</td>
-                                    <td data-label="Sub Role" >{{ _.map(user.data.vendor,'sub_role').join(',') }}</td>
-                                 
-                                    <td data-label="Vendor Name">{{ _.map(user.data.uservendor,'name').join(',') }}</td>
-                                    <td data-label="Action">
-                                        <div class="mt-4">
-                                            <button @click.prevent="showEditModal(user)" class="border-2 outline-none hover:bg-green-600 border-green-600 rounded mx-2 px-2 py-1 hover:text-white mb-3">Edit</button>
-                                            <button  class="border-2 outline-none hover:bg-red-600 border-red-600 rounded mx-2 px-2 py-1 hover:text-white">Permission</button>
-                                        </div>
-                                    </td>
+                    <tr v-for="(user, index) in props.users.data" :key="index">
+                        <td>{{ ++index }}</td>
+                        <td data-label="Name">{{ user.data.name }} </td>
+                        <td data-label="Email">{{ user.data.email }}</td>
+                        <td data-label="Role">{{ user.data.role }}</td>
+                        <td data-label="Sub Role">{{ _.map(user.data.vendor, 'sub_role').join(',') }}</td>
+
+                        <td data-label="Vendor Name">{{ _.map(user.data.uservendor, 'name').join(',') }}</td>
+                        <td data-label="Action">
+                            <div class="mt-4">
+                                <button @click.prevent="showEditModal(user)"
+                                    class="border-2 outline-none hover:bg-green-600 border-green-600 rounded mx-2 px-2 py-1 hover:text-white mb-3">Edit</button>
+                                <button
+                                    class="border-2 outline-none hover:bg-red-600 border-red-600 rounded mx-2 px-2 py-1 hover:text-white">Permission</button>
+                            </div>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -163,7 +165,7 @@ const closeEditModal = () => {
                 </h2>
 
                 <div class="mt-6">
-                    <InputLabel for="username" value="User Name"  />
+                    <InputLabel for="username" value="User Name" />
                     <TextInput id="name" ref="nameRef" v-model="form.name" type="text" class="mt-1 block w-full"
                         placeholder="Username" />
                     <InputError :message="form.errors.name" class="mt-2" />
@@ -178,115 +180,87 @@ const closeEditModal = () => {
                     <InputError :message="form.errors.email" class="mt-2" />
                 </div>
 
-                        <div class="mt-6">
-                            <InputLabel for="password" value="text" class="sr-only" />
-                        <TextInput
-                            id="password"
-                            ref="passwordRef"
-                            v-model="form.password"
-                            type="password"
-                            class="mt-1 block w-full"
-                            placeholder="Password"
-                        />
-                        <InputError :message="form.errors.password" class="mt-2" />
-                        </div>
-                        <div class="mt-6">
-                            <InputLabel for="Role" value="text" class="sr-only" />
-                            <select  v-model="form.role" class="capitalize mt-1 block w-full border-1 border-gray-300 rounded">
-                                <option  class="capitalize"
-                                    v-for="role in props.roles"
-                                    :value="role.role" 
-                                >
-                                <span class="capitalize">{{ role.role }}</span>
-                                </option>
-                            </select>
-                            <InputError :message="form.errors.role" class="mt-2" />
-                        </div>
-                        
-                        <div v-if="form.role === 'vendor'">
-                            <div class="mt-6">
-                                <select v-model="form.vendor_name" class="capitalize mt-1 block w-full border-1 border-gray-300 rounded">
-                                    <option 
-                                        v-for="vendor in props.vendors"
-                                        :value="vendor.id" class="capitalize"
-                                    >
-                                    <span class="capitalize">{{ vendor.name }}</span>
+                <div class="mt-6">
+                    <InputLabel for="password" value="text" class="sr-only" />
+                    <TextInput id="password" ref="passwordRef" v-model="form.password" type="password"
+                        class="mt-1 block w-full" placeholder="Password" />
+                    <InputError :message="form.errors.password" class="mt-2" />
+                </div>
+                <div class="mt-6">
+                    <InputLabel for="Role" value="text" class="sr-only" />
+                    <select v-model="form.role" class="capitalize mt-1 block w-full border-1 border-gray-300 rounded">
+                        <option class="capitalize" v-for="role in props.roles" :value="role.role">
+                            <span class="capitalize">{{ role.role }}</span>
+                        </option>
+                    </select>
+                    <InputError :message="form.errors.role" class="mt-2" />
+                </div>
 
-                                    </option>
-                                </select>
-                                <InputError :message="form.errors.user_type" class="mt-2" />
-                            </div>
-                            <div class="mt-6">
-                                <select v-model="form.sub_role" class="capitalize mt-1 block w-full border-1 border-gray-300 rounded">
-                                    <option 
-                                        v-for="sub_role in props.sub_roles"
-                                        :value="sub_role.sub_role" class="capitalize"
-                                    >
-                                    <span class="capitalize">{{ sub_role.sub_role }}</span>
+                <div v-if="form.role === 'vendor'">
+                    <div class="mt-6">
+                        <select v-model="form.vendor_name"
+                            class="capitalize mt-1 block w-full border-1 border-gray-300 rounded">
+                            <option v-for="vendor in props.vendors" :value="vendor.id" class="capitalize">
+                                <span class="capitalize">{{ vendor.name }}</span>
 
-                                    </option>
-                                </select>
-                                <InputError :message="formEdit.errors.sub_role" class="mt-2" />
-                            </div>
-
-                        </div>
-
-                        <div class="mt-6 flex justify-end">
-                            <SecondaryButton @click.prevent="closeModal"> Cancel </SecondaryButton>
-                            <PrimaryButton
-                                class="ml-3"
-                                :class="{ 'opacity-25': form.processing }"
-                                :disabled="form.processing"
-                                @click.prevent="createUser"
-                            >
-                                Add
-                            </PrimaryButton>
-                        </div>
+                            </option>
+                        </select>
+                        <InputError :message="form.errors.user_type" class="mt-2" />
                     </div>
+                    <div class="mt-6">
+                        <select v-model="form.sub_role"
+                            class="capitalize mt-1 block w-full border-1 border-gray-300 rounded">
+                            <option v-for="sub_role in props.sub_roles" :value="sub_role.sub_role" class="capitalize">
+                                <span class="capitalize">{{ sub_role.sub_role }}</span>
+
+                            </option>
+                        </select>
+                        <InputError :message="formEdit.errors.sub_role" class="mt-2" />
+                    </div>
+
+                </div>
+
+                <div class="mt-6 flex justify-end">
+                    <SecondaryButton @click.prevent="closeModal"> Cancel </SecondaryButton>
+                    <PrimaryButton class="ml-3" :class="{ 'opacity-25': form.processing }" :disabled="form.processing"
+                        @click.prevent="createUser">
+                        Add
+                    </PrimaryButton>
+                </div>
+            </div>
         </Modal>
-        <Modal :show="confirmShowEditModal"  @close="closeEditModal">
-                    <div class="p-6">
-                        <h2 class="text-lg font-medium text-gray-900">
-                            Edit User
-                        </h2>
-                        <div class="mt-6">
-                            <InputLabel for="Vendor Name" value="text" class="sr-only" />
-                            <select v-model="formEdit.vendor_id" class="mt-1 block w-full border-gray-300 rounded">
-                                <option 
-                                        v-for="vendor in props.vendors"
-                                        :value="vendor.id"  class="capitalize"
-                                    >
-                                    <span class="capitalize">{{ vendor.name }}</span>
+        <Modal :show="confirmShowEditModal" @close="closeEditModal">
+            <div class="p-6">
+                <h2 class="text-lg font-medium text-gray-900">
+                    Edit User
+                </h2>
+                <div class="mt-6">
+                    <InputLabel for="Vendor Name" value="text" class="sr-only" />
+                    <select v-model="formEdit.vendor_id" class="mt-1 block w-full border-gray-300 rounded">
+                        <option v-for="vendor in props.vendors" :value="vendor.id" class="capitalize">
+                            <span class="capitalize">{{ vendor.name }}</span>
 
-                                </option>
-                            </select>
-                        </div>
-                        <div class="mt-6">
-                            <InputLabel for="Sub Role" value="text" class="sr-only" />
-                            <select v-model="formEdit.sub_role" class="mt-1 block w-full border-gray-300 rounded">
-                                <option 
-                                        v-for="sub_role in props.sub_roles"
-                                        :value="sub_role.sub_role" class="capitalize"
-                                    >
-                                    <span class="capitalize">{{ sub_role.sub_role }}</span>
+                        </option>
+                    </select>
+                </div>
+                <div class="mt-6">
+                    <InputLabel for="Sub Role" value="text" class="sr-only" />
+                    <select v-model="formEdit.sub_role" class="mt-1 block w-full border-gray-300 rounded">
+                        <option v-for="sub_role in props.sub_roles" :value="sub_role.sub_role" class="capitalize">
+                            <span class="capitalize">{{ sub_role.sub_role }}</span>
 
-                                </option>
-                            </select>
-                        </div>
-                        <div class="mt-6 flex justify-end">
-                            <SecondaryButton @click.prevent="closeEditModal"> Cancel </SecondaryButton>
+                        </option>
+                    </select>
+                </div>
+                <div class="mt-6 flex justify-end">
+                    <SecondaryButton @click.prevent="closeEditModal"> Cancel </SecondaryButton>
 
-                            <PrimaryButton
-                                class="ml-3"
-                                :class="{ 'opacity-25': formEdit.processing }"
-                                :disabled="formEdit.processing"
-                    
-                                @click="editUser"
-                            >
-                                Update
-                            </PrimaryButton>
-                        </div>
-                    </div>
-                </Modal> 
+                    <PrimaryButton class="ml-3" :class="{ 'opacity-25': formEdit.processing }"
+                        :disabled="formEdit.processing" @click="editUser">
+                        Update
+                    </PrimaryButton>
+                </div>
+            </div>
+        </Modal>
     </LayoutAuthenticated>
 </template>
